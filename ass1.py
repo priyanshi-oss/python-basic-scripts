30c5a41ac5e4c2df6fdb9e4cafcf967d219ae023
priyanshi-oss <priyanshichouksey9031@gmail.com>
2025-05-17 21:24:11 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,24 @@
+#task 1
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+addition=a+b
+substraction=a-b
+multiplication=a*b
+division=a/b
+print("addition:",addition)
+print("substraction:",substraction)
+print("multiplication:",multiplication)
+print("division:",division)
+
+
+#task2
+
+# Take user's first and last name as input
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+
+# Concatenate to form full name
+full_name = firstname + " " + lastname
+
+# Print personalized greeting
+print("Hello,", full_name + "! Welcome to the Python world.")
